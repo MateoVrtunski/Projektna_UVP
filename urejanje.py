238716ff392a975fa969bf337d9190ffdbdea5f0
Mateo Vrtunski <mv4662@student.uni-lj.si>
2023-08-23 11:18:55 +0200

    dodal se stevilo iger

--- urejanje.py
+++ urejanje.py
@@ -42,15 +42,23 @@ for i in range(len(civilizacije)):
     seznam.append(re.findall(vzorec_procentov,civ))
     seznam = seznam[0]
 
+    igre = []
+    vzorec_iger = r"<p class=\"text-sm\">\d+,\d{3} picks"
+    igre.append(re.findall(vzorec_iger,civ))
+    igre = igre[0]
+    igre[0] = igre[0].replace('<p class="text-sm">','')
+    igre[0] = igre[0].replace(' picks','')
+
+
     baza = {}
     baza['civilizacija'] = f'{civilizacije[i]}'
     baza['zmagovalna stopnja'] = seznam[0]
     baza['igralna stopnja'] = seznam[3]
+    baza['stevilo iger'] = igre[0]
     glavni_seznam.append(baza)
 
+ 
 
 
-
-    
 
 print(glavni_seznam)
